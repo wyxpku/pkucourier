d07c4f3c340241a3b2ea2df1094bc26fdc3a6394
wyxpku <wyx0507@gmail.com>
2015-12-16 17:26:50 +0800

    add view models and views

--- will/models.py
+++ will/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from user.models import User
 
 # Create your models here.
+class Will(models.Model):
+    pfrom = models.CharField(max_length = 50)
+    pto = models.CharField(max_length = 50)
+    info = models.CharField(max_length = 50)
+    build_time = models.DateTimeField()
+    owner = models.ForeignKey(User, related_name = 'will')
+    status = models.IntegerField(default=0) # 0->Opened, 1->Closed
+    def to_dict(self):
+        return dict(
+            id = self.id, pfrom = self.pfrom, pto = self.pto, info = self.info,
+            build_time = str(self.build_time), owner = self.owner
+        )
+
--- will/urls.py
+++ will/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'will/new/', views.new, name='newwill'),
+    url(r'^new/', views.new, name='newwill'),
+    url(r'^(\d+)/', views.get_info, name='getWillInfo')
 ]
