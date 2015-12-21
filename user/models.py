7a51e39d89edd2539487ac8370794586bcc489dc
Wen Jiu <mercury_wj@163.com>
2015-12-21 09:42:01 +0800

    change files' mod, repair some bugs

--- user/models.py
+++ user/models.py
@@ -12,10 +12,11 @@ class User(models.Model):
     bonus = models.IntegerField(default = 0)
     hx_username = models.CharField(max_length = 50, null = True)
     hx_password = models.CharField(max_length = 50, null = True)
+    avatar = models.IntegerField(default=0)
 
     def to_dict(self):
         return dict(
-            uid = self.id, name = self.name, email = self.email, status = self.status, avatar = self.avator,
+            uid = self.id, name = self.name, email = self.email, status = self.status, avatar = self.avatar,
             signup_time = self.signup_time.strftime('%Y-%m-%d %H:%M:%S'),
             bonus = self.bonus, hx_username = self.hx_username, hx_password = self.hx_password
         )
--- will/urls.py
+++ will/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^new/', views.new, name='newwill'),
     url(r'^(\d+)/', views.get_info, name='getWillInfo'),
     url(r'^all/', views.all, name='allwill'),
+    url(r'^delete/$', views.delete_will, name='deleteWill'),
 ]
